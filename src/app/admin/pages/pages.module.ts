413744ab6795717b6f28161d2d38017a0c60e98c
Pavel Tretyakov <tretyakovpavel.ru@gmail.com>
2019-03-04 02:40:36 +0300

    Requried tested ngx-widget-grid

--- src/app/admin/pages/pages.module.ts
+++ src/app/admin/pages/pages.module.ts
@@ -1,16 +1,22 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { NgxWidgetGridModule } from 'ngx-widget-grid';
 
 import { PagesRoutingModule } from './pages-routing.module';
 import { PagesComponent } from './pages.component';
 import { PageComponent } from './page/page.component';
 import { PageListComponent } from './page-list/page-list.component';
+import { MaterialModule } from 'src/app/material.module';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [PagesComponent, PageComponent, PageListComponent],
   imports: [
+    FormsModule,
     CommonModule,
-    PagesRoutingModule
+    PagesRoutingModule,
+    MaterialModule,
+    NgxWidgetGridModule,
   ]
 })
 export class PagesModule { }
