788354f1def3759f18fd9c9d674bf8ee32a730a1
Pavel Tretyakov <1@tretyakovpavel.ru>
2019-03-16 01:13:21 +0300

    Initialized widget-player module and component

---
+++ src/app/shared/components/widget-players/widget-players.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-widget-players',
+  templateUrl: './widget-players.component.html',
+  styleUrls: ['./widget-players.component.scss']
+})
+export class WidgetPlayersComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
---
+++ src/app/shared/components/widget-players/widget-players.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { WidgetPlayersComponent } from './widget-players.component';
+
+@NgModule({
+  declarations: [WidgetPlayersComponent],
+  imports: [
+    CommonModule
+  ]
+})
+export class WidgetPlayersModule { }
